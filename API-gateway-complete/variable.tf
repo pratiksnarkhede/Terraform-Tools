924caa6664df79f50e0386a6d29045ec6c66c55e
Pratik Narkhede <89267289+pratiksnarkhede@users.noreply.github.com>
2023-09-21 13:31:46 +0530

    Update variable.tf

--- API-gateway-complete/variable.tf
+++ API-gateway-complete/variable.tf
@@ -3,22 +3,22 @@ variable "myregion" {
 }
 
 variable "accountId" {
-  default = "438233270299"
+  default = "<Account_id>" # Replace Account_id with your Account id
 }
 
 
 # Define your custom domain name
 variable "custom_domain_name" {
-  default = "mywebapplication.me" 
+  default = "<domain_name>" # Replace domain_name with your domain name
 }
 
 # Define your existing SSL certificate ARN
 variable "ssl_certificate_arn" {
-  default = "arn:aws:acm:us-east-1:438233270299:certificate/01cbf241-03ed-4d6f-a505-133f2321c8ed"  # Replace with your SSL certificate ARN
+  default = "<certificate arn>"  # Replace with your SSL certificate ARN
 }
 
 # Define your hosted zone ID
 variable "hosted_zone_id" {
-  default = "Z028009242LYGS6EC2PA"  # Replace with your hosted zone ID
+  default = "<hosted_zone>"  # Replace with your hosted zone ID
 }
 
